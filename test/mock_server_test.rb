5beb0a3c7a605e9150300655eadca93bfccea72d
German Monfort <german.monfort@gmail.com>
2012-01-02 18:22:59 -0200

    adding configuration support

--- test/mock_server_test.rb
+++ test/mock_server_test.rb
@@ -29,18 +29,19 @@ end
 
 class MockServerRackBuilderTest < Test::Unit::TestCase
   def setup
-    @server = MockServerThin.new(HelloWorldRackBuilder, 4001)
+    @server = MockServerThin.new(HelloWorldRackBuilder, :port => 4001)
     @server.start
   end
 
   def test_server
     assert_equal "Rackup!", open("http://localhost:4001").read
-  end end
+  end
+end
 
 class MockServerMethodsTest < Test::Unit::TestCase
   extend MockServerThin::Methods
 
-  mock_server(4002) {
+  mock_server(:port => 4002) {
     get "/" do
       "Goodbye"
     end
@@ -48,5 +49,23 @@ class MockServerMethodsTest < Test::Unit::TestCase
 
   def test_server
     assert_equal "Goodbye", open("http://localhost:4002").read
+  end
+end
+
+class MockServerConfigTest < Test::Unit::TestCase
+  def setup
+    MockServerThin.configure do |config|
+      config.port = 5067
+      config.timeout = 10
+      config.host = "127.0.0.1"
+    end
+    @server = MockServerThin.new(HelloWorldSinatra)
+    @server.start
+  end
+
+  def test_server_config
+    assert_equal 5067, @server.config.port
+    assert_equal 10, @server.config.timeout
+    assert_equal "127.0.0.1", @server.config.host
   end
 end
